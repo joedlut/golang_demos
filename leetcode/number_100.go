05e3cc49ffe5c52858087c3b3a060bb6bdf19206
joedlut <2532771263@qq.com>
2023-06-21 22:48:37 +0800

    add files

--- leetcode/number_100.go
+++ leetcode/number_100.go
@@ -4,13 +4,14 @@ package main
 给你两棵二叉树的根节点 p 和 q ，编写一个函数来检验这两棵树是否相同。
 如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的。
 */
-type TreeNode struct {
+
+type TreeNode1 struct {
 	Val   int
-	Left  *TreeNode
-	Right *TreeNode
+	Left  *TreeNode1
+	Right *TreeNode1
 }
 
-func isSameTree(p *TreeNode, q *TreeNode) bool {
+func isSameTree(p *TreeNode1, q *TreeNode1) bool {
 	if p == nil && q == nil {
 		return true
 	}
@@ -24,4 +25,8 @@ func isSameTree(p *TreeNode, q *TreeNode) bool {
 		return isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
 	}
 	return false
+}
+
+func main() {
+
 }
